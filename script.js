8238c80b456043fd4e76013f9ba29bdc4cb766eb
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-12 10:47:30 +0300

    working on delete element function

--- script.js
+++ script.js
@@ -2,48 +2,75 @@ const listEl = document.getElementById('list')
 const todosEl = document.getElementById('todos')
 const doneEl = document.getElementById('done')
 const deletedEl = document.getElementById('delete')
+const pendingEl = document.getElementById('pending')
 
-const deletedList = []
-let doneTodos = 1;
 
-let deleted = 0;
+
+
+let total = [
+    {
+        title: 'Design TODO Application UI',
+        done: false,
+        assignee: 'Ahmad ilawa',
+        editable: true
+
+    },
+    {
+        title: 'Design TODO Application UI',
+        done: true,
+        assignee: 'Amin Nasser',
+        editable: true
+
+    }
+]
+
+
+
 
 function rerender() {
-    doneEl.innerHTML = doneTodos
-    deletedEl.innerHTML = deletedList.length
+    let pending = 0
+    let done = 0
+    let deleted = 0;
+    let elements = ""
+    for(let i=0; i<total.length; i++){
+        if(total[i].done){
+            done+=1
+        }else{
+            pending +=1
+        }
+        elements+= `
+        <div class="task ${total[i].done ? "done": ''}" key=${i}>
+        <i class="fa-solid fa-check check-box" onclick="check(event)"></i>
+        <div class="task-body">
+            <input type="text" value="${total[i].title}" ${total[i].editable ? "disabled":""}>
+            <p>${total[i].assignee}</p>
+        </div>
+        <div class="control">
+            <i class="fa-solid fa-pen-to-square" onclick="edit(event)"></i>
+            <i class="fa-solid fa-trash" onclick="deleteEl(event)"></i>
+        </div>
+    </div>
+        `
+    }
+    listEl.innerHTML = elements
+    doneEl.innerText = done
+    deletedEl.innerText = deleted
+    pendingEl.innerText = pending
     todosEl.innerText = listEl.children.length
+
 
 }
 
 function check(event) {
-    const index = Array.from(
-        listEl.children
-    ).indexOf(event.target.parentElement);
-    const el = listEl.children[index]
+   total[event.target.parentElement.getAttribute('key')].done = !total[event.target.parentElement.getAttribute('key')].done 
 
-    el.children[1].children[0].disabled = true
-    if (el.classList[1]) {
-        el.classList.remove('done')
-        doneTodos--;
-    }
-    else {
-        doneTodos++;
-        el.classList.add('done')
-    }
+
     rerender()
 }
 function deleteEl(event) {
-    const index = Array.from(
-        listEl.children
-    ).indexOf(event.target.parentElement.parentElement);
 
-    const el = listEl.children[index]
-    if(el.classList[1]){
-        doneTodos--;
-    }
-    deletedList.splice(0, 0, el)
-    
-    el.remove()
+    let x = event.target.parentElement.parentElement.getAttribute('key')
+    total = total.filter((p, i) => i != x)
     rerender()
 }
 
@@ -76,4 +103,5 @@ addEventListener('keydown', (e) => {
         }
     }
 })
+
 rerender()
